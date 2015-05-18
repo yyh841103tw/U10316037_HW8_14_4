14a7b7e1a73b24fc6b037acf19b3eccd816d4fc0
yyh841103tw <yyh841103tw@gmail.com>
2015-05-18 09:50:48 +0800

    Create HM.java

---
+++ HM.java
@@ -0,0 +1,87 @@
+import java.lang.IllegalArgumentException;
+
+public class HM{
+	//main method
+	public static void main(String[] args) {
+		try {
+			Loan spirit = new Loan(-1, 2, 2);
+		}
+		catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+}
+
+class Loan {
+	
+	private double annualInterestRate;
+	private int numOfYears;
+	private double loanAmount;
+	private java.util.Date loanDate;
+
+	/** Default constructor */
+	public Loan() {
+		this(2.5, 1, 1000);
+	}
+
+	/** Construct a NewLoan with specified annual interest rate, number of years and loan amount*/
+	public Loan(double annualInterestRate, int numOfYears,double loanAmount) {
+		if (annualInterestRate <= 0)
+			throw new IllegalArgumentException("annualInterestRate is negative");
+		if (numOfYears <= 0)
+			throw new IllegalArgumentException("NumberOfYears is negative");
+		if (loanAmount <= 0)
+			throw new IllegalArgumentException("LoanAmount is negative");
+
+	setAnnualInterestRate(annualInterestRate);
+	setNumOfYears(numOfYears);
+	setLoanAmount(loanAmount);
+	}
+	
+	/** Return annualInterestRate */
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	/** Set a new annualInterestRate */
+	public void setAnnualInterestRate(double annualInterestRate) {
+		if (annualInterestRate <= 0)
+			throw new IllegalArgumentException("Annual interest rate must be positive.");
+		this.annualInterestRate = annualInterestRate;
+	}
+	/** Return numOfYears */
+	public int getNumOfYears() {
+		return numOfYears;
+	}
+	/** Set a new numOfYears */
+	public void setNumOfYears(int numOfYears) {
+		if (numOfYears <= 0)
+			throw new IllegalArgumentException("Number of years must be positive.");
+			this.numOfYears = numOfYears;
+	}
+	/** Return loanAmount */
+	public double getLoanAmount() {
+		return loanAmount;
+	}
+	/** Set a newloanAmount */
+	public void setLoanAmount(double loanAmount) {
+		if (loanAmount <= 0)
+			throw new IllegalArgumentException("Loan amount must be positive.");
+		this.loanAmount = loanAmount;
+	}
+	/** Find monthly payment */
+	public double monthlyPayment() {
+		double monthlyInterestRate = annualInterestRate / 1200;
+		return loanAmount * monthlyInterestRate / (1 -
+		(Math.pow(1 / (1 + monthlyInterestRate), numOfYears * 12)));
+	}
+	/** Find total payment */
+	public double totalPayment() {
+		return monthlyPayment() * numOfYears * 12;
+	}
+	//return loan date
+	public java.util.Date getLoanDate(){
+		return loanDate;
+	}
+}
+
+
